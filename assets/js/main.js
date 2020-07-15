612602b818cffac92a1eb016264de13539ecf9ed
shahadatalam2020 <67002334+shahadatalam2020@users.noreply.github.com>
2020-07-15 17:05:48 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(50).fadeOut('slow', function() {
+      $('#preloader').delay(0).fadeOut('slow', function() {
         $(this).remove();
       });
     }
